a5b315da71c3d767fc1e86fe697f713ee55d8716
ncdarkgenuis <ncdarkgenuis@gmail.com>
2026-05-19 02:19:11 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static frontend files cleanly from the root directory
 app.use(express.static(path.join(__dirname)));
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
